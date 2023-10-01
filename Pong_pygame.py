defa77e0adb71d4dc10bed77600ba2fc5b3fc6a2
LeanViko <leangant@gmail.com>
2023-10-01 16:21:30 -0300

    Estructura principal

---
+++ Pong_pygame.py
@@ -0,0 +1,20 @@
+import pygame
+import sys
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen_widht = 1280
+screen_height = 960
+screen = pygame.display.set_mode((screen_widht, screen_height))
+pygame.display.set_caption('Pong re piola')
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.flip()
+    clock.tick(60)
