78b23a2e7b93284b9d7e150485b7115d4ac30102
LeanViko <leangant@gmail.com>
2023-10-02 10:29:06 -0300

    Animacion y colisiones con la pelota

--- Pong_pygame.py
+++ Pong_pygame.py
@@ -1,6 +1,21 @@
 import pygame
 import sys
 
+def ball_animation( ):
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+    
+    
+
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -9,6 +24,7 @@ screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong re piola')
 
+#Define rectangles
 ball = pygame.Rect((screen_width/2-15), (screen_height/2-15),30,30)
 player = pygame.Rect((screen_width - 20), (screen_height/2-70),20,140)
 opponent = pygame.Rect(10, (screen_height/2-70),20,140)
@@ -16,14 +32,21 @@ opponent = pygame.Rect(10, (screen_height/2-70),20,140)
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 
+# ball speed
+ball_speed_x = 7
+ball_speed_y = 7
+
+
 while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    ball_animation()
     
-    #Visuals
+    #Draw rectangles 
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey,player)
     pygame.draw.rect(screen, light_grey,opponent)
