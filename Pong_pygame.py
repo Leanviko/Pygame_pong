3bc66bf6af4b080fb8e931b3cb7989c810819d3e
LeanViko <leangant@gmail.com>
2023-10-02 10:02:57 -0300

    Rectangulos oponentes y pelota

--- Pong_pygame.py
+++ Pong_pygame.py
@@ -4,10 +4,17 @@ import sys
 pygame.init()
 clock = pygame.time.Clock()
 
-screen_widht = 1280
+screen_width = 1280
 screen_height = 960
-screen = pygame.display.set_mode((screen_widht, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong re piola')
+
+ball = pygame.Rect((screen_width/2-15), (screen_height/2-15),30,30)
+player = pygame.Rect((screen_width - 20), (screen_height/2-70),20,140)
+opponent = pygame.Rect(10, (screen_height/2-70),20,140)
+
+bg_color = pygame.Color('grey12')
+light_grey = (200,200,200)
 
 while True:
 
@@ -15,6 +22,13 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
+    #Visuals
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey,player)
+    pygame.draw.rect(screen, light_grey,opponent)
+    pygame.draw.ellipse(screen, light_grey,ball)
+    pygame.draw.aaline(screen, light_grey,(screen_width/2,0),(screen_width/2,screen_height))
 
     pygame.display.flip()
     clock.tick(60)
